727492704949b759facd74d0d200d8c41114b9a8
DAT14V1Elvis <elvi0042@stud.kea.dk>
2017-04-22 14:29:01 +0200

    All Models Work

---
+++ MyBookList/Models/BookGenre.cs
@@ -0,0 +1,19 @@
+﻿using MyBookList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBookList.Models
+{
+    public class BookGenre
+    {
+        public int ID { get; set; }
+
+        public int BookID { get; set; }
+        public virtual Book Book { get; set; }
+
+        public int GenreID { get; set; }
+        public virtual Genre Genre { get; set; }
+    }
+}
